e6083de9c874f43a71dd2b3cde5d24a965b2ef44
ariefouren <aporan@ono.ac.il>
2023-08-20 12:26:24 +0300

    20-08

--- Less_12_ex_05_dynamic_cast_mult_inh/DynamicCastingAndMultInh.cpp
+++ Less_12_ex_05_dynamic_cast_mult_inh/DynamicCastingAndMultInh.cpp
@@ -8,7 +8,17 @@ using namespace std;
 
 using namespace std;
 
-class Vehicle {
+class Machine {
+public:
+    Machine(double weight = 1000) : weight(weight) {};
+    virtual double getWeight() const { return weight; };
+
+protected: 
+    double weight;
+
+};
+
+class Vehicle: virtual public Machine {
     static int numVehicles;
 
 public:
@@ -27,16 +37,15 @@ public:
     int getSerialNumber() const {
         return serialNumber;
     }
-    string toString() const {
+    virtual string toString() const {
         return "Vehicle: serial number = " + serialNumber;
     }
 
 protected:
     int serialNumber;
-
 };
 
-class Car : public Vehicle {
+class Car : virtual public Vehicle {
 protected:
     string brand;
 
@@ -50,7 +59,8 @@ public:
     }
 
     virtual string toString() const {
-        return "Car: Brand=" + brand;
+        string str = "Car: serial number = " + getSerialNumber();
+        return str;
     }
 
     ~Car() {
@@ -72,7 +82,8 @@ public:
     }
 
     virtual string toString() const {
-        return "Airplane: Model=" + model;
+        return "Airplane: " + Vehicle::toString() + ", " +
+            "Model = " + model;
     }
 
     ~Airplane() {
@@ -99,7 +110,9 @@ public:
 
 
     virtual string toString() const {
-        return Car::toString() + ", " + Airplane::toString();
+        return "Flying car: " + 
+            Car::toString() + ", " + 
+            Airplane::toString();
     }
 
 
@@ -113,15 +126,78 @@ int Vehicle::numVehicles = 0;
 int main() {
     FlyingCar* flyingCarPtr = 
         new FlyingCar("XYZ Motors", "Flying Carpet 2023");
+   
+    // for dynamic cast, the pointer should be to a polymorphic class
+    // (class with at least one virtual function)
     
+    // demonstrating dynamic upcast
+    cout << "\n=== demonstrating dynamic upcast ===";
+    
+    // OK. upcasting from FlyinCar* to Vehicle* 
+    // through Car* is unambiguous 
     Car* carPtr = dynamic_cast<Car*>(flyingCarPtr); 
     Vehicle* vehiclePtr1 = dynamic_cast<Vehicle*>(carPtr);
 
-    Airplane* airplanePtr = dynamic_cast<Airplane*>(flyingCarPtr);
-    Vehicle* vehiclePtr2 = dynamic_cast<Vehicle*>(airplanePtr);
+    // OK. upcasting from FlyingCar* to Vehicle* 
+    // through Airplane* is unambiguous 
+    Vehicle* vehiclePtr2 = dynamic_cast<Vehicle*>
+        (dynamic_cast<Airplane*>(flyingCarPtr));
 
-    // ERROR ! Base class "Vehicle" is ambiguous
-    Vehicle* vehiclePtr3 = dynamic_cast<Vehicle*>(flyingCarPtr);
+    // OK. the upcasting from FlyingCar* to Machine* is unambiguous.
+    // there is only one copy of Machine object in 
+    // FlyingCar object due to virtual inheritance from Machine
+    Machine* machinePtr = dynamic_cast<Machine*>(flyingCarPtr);
 
-    return 0;
+    // ERROR ! upcasting to base class "Vehicle" is ambiguous.
+    // inheritance of Car and Airplane from Vehicle is not virtual, therefore 
+    // there are two objects that represent Vehicle in FlyingCar
+    // Vehicle* vehiclePtr3 = dynamic_cast<Vehicle*>(flyingCarPtr);
+
+    
+    // demonstrating dynamic downcast
+    cout << "=== demonstrating dynamic downcast ===\n";
+    // OK, since FlyingCar is a Machine
+    machinePtr = flyingCarPtr; 
+
+    // ERROR ! downcast from Machine* to Vehicle* is incorrect
+    // and generates a run-time error
+    // vehiclePtr1 = dynamic_cast<Vehicle*>(machinePtr);
+    // cout << vehiclePtr1->toString() << endl;
+
+    // OK. downcast from Machine* to Car* is unambiguous  
+    carPtr = dynamic_cast<Car*>(machinePtr);
+
+    // OK. downcast from Machine* to FlyingCar* is unambiguous  
+    FlyingCar* flyingCarPtr2 = dynamic_cast<FlyingCar*>(machinePtr);
+    
+    cout << "\nFlyingCar: Car serial number: " <<
+        flyingCarPtr2->Car::getSerialNumber() << endl;
+
+    cout << "FlyingCar: Airplane serial number: " <<
+        flyingCarPtr2->Airplane::getSerialNumber() << endl<< endl;
+
+    // 
+    vehiclePtr1 = dynamic_cast<Vehicle*>
+        (dynamic_cast<Car*>
+            (dynamic_cast<FlyingCar*>(machinePtr)));
+    cout << "Vehicle (trough Car): serial number = " 
+        << vehiclePtr1->getSerialNumber() << endl;
+         
+    // 
+    vehiclePtr2 = dynamic_cast<Vehicle*>
+        (dynamic_cast<Airplane*>
+            (dynamic_cast<FlyingCar*>(machinePtr)));
+    cout << "Vehicle (trough Airplane): serial number = " 
+        << vehiclePtr2->getSerialNumber() << endl;
+
+    // demonstrating cross-casting
+    cout << "\n=== demonstrating cross-casting ===\n";
+    carPtr = dynamic_cast<Car*>(flyingCarPtr);
+    cout << "Car: serial number = "
+        << carPtr->getSerialNumber() << endl;
+
+    Airplane* airplanePtr = dynamic_cast<Airplane*>(carPtr);
+    cout << "Airplane: serial number = "
+        << airplanePtr->getSerialNumber() << endl;
+   return 0;
 }
