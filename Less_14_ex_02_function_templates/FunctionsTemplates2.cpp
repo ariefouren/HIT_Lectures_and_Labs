c500f7ef02349a16be974d46bf69ecd100729ff2
ariefouren <aporan@ono.ac.il>
2023-08-26 23:59:09 +0300

    26-08

--- Less_14_ex_02_function_templates/FunctionsTemplates2.cpp
+++ Less_14_ex_02_function_templates/FunctionsTemplates2.cpp
@@ -36,38 +36,3 @@ int main()
 		<< quotient(x, y) << endl;
 }
 
-//
-//// ==========================
-//// 
-//// function template with default type and default argument value
-//template<class T, class U = double>
-//T Exp(T power, U base = M_E )
-//{
-//	return pow(base, power);
-//}
-//
-//// class template with default type 
-//template<class T = double>
-//class Complex
-//{
-//private:
-//	T re, im;
-//};
-//
-//int main()
-//{
-//	//cout << "sum<double, int>(y, n) = " << sum<double, int>(y, n) << endl;
-//	//cout << "sum(y, n) = " << sum(y, n) << endl;
-//
-//	//cout << "sum<int, double>(n, y) = " << sum<int, double>(n, y) << endl;
-//	//// if template specialization and a function 
-//	//// equally match, the function is preferred
-//	//cout << "sum(n, y) = " << sum(n, y) << endl;
-//
-//	cout << "Exp(1.0) = " << Exp(1.0, 1) << endl;
-//	cout << "Exp(1.0) = " << Exp(1.0) << endl;
-//	cout << "Exp(1, M_PI) = " << Exp(1, M_PI) << endl;
-//
-//	Complex<int> intComplex;
-//	Complex<> doubleComplex;
-//}
